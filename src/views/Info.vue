3a2b34330e6847c351baa922a4a4a17ad8bf7968
iga33087 <iga33087@gmail.com>
2022-04-11 16:26:28 +0800

    999

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -10,13 +10,13 @@
       <Label title="聯絡方式" text="0903615478"></Label>
       <Label title="居住地區" text="新北市板橋區"></Label>
       <Label title="希望待遇" text="面議"></Label>
-      <Label title="工作經歷" text="前端工程師 (小驢行股份有限公司) 2019/8/20 ~ 2020/09/30"></Label>
+      <Label title="工作經歷" text="前端工程師 (小驢行股份有限公司) 2019/8/20 ~ 2020/9/30<br>前端工程師 (中華龍網股份有限公司) 2021/3/2 ~ 2022/4/6"></Label>
       <Label title="可工作範圍" text="新北市、台北市"></Label>
       <Label title="學歷" text="華夏科技大學 - 資訊管理系 - 五專部"></Label>
     </div>
     <div class="indexItem SKILLS animate__animated" v-scrollIn="'animate__bounceIn'">
       <Title content="SKILLS"></Title>
-      <Label title="工作技能" text="HTML5、CSS3、SCSS、Javascript、AJAX、Vue、Vuex、Webpack、Git、NodeJS、WebSocket、MySQL、PHP"></Label>
+      <Label title="工作技能" text="HTML5、CSS3、SCSS、Javascript、AJAX、Vue、Vuex、Webpack、Git、NodeJS、WebSocket、MySQL、MongoDB、PHP、Linux、Docker、CI/CD、AWS"></Label>
       <div style="display:flex;flex-wrap:wrap;">
         <Label icon="icon-check-square-regular" title="開發過大型系統" style="width:calc(100% / 3);" width="100%"></Label>
         <Label icon="icon-check-square-regular" title="可獨立作業" style="width:calc(100% / 3);" width="100%"></Label>
@@ -25,7 +25,10 @@
     </div>
     <div class="indexItem PROFILE animate__animated" v-scrollIn="'animate__bounceIn'">
       <Title content="PROFILE"></Title>
-      <div class="PROFILEContent">我是畢業於華夏科技大學資管系五專部的鄭愉，就讀五專期間對程式設計及網頁深感興趣，因此開始自學前端技術，如HTML、CSS、Javascript、AJAX、PHP等技術... 並且在五專四年級的專題報告應用所學，一個人打造了個"線上測驗平台"，並且獲得評審高分，專題報告分數為全班第二。<br><br>五專畢業以後進入了小驢行股份有限公司，開始接觸了前端框架Vue，以及各種前端的前沿技術，比如動態路由、打包及模組化設計等等... 在那邊也參與開發過許多專案，包括大型系統的前台、後台的獨立開發，經過一年的磨練學會了非常多東西、克服了非常多困難，為我前端身涯當中非常重要的里程碑。</div>
+      <div class="PROFILEContent">
+        我是畢業於華夏科技大學資管系五專部的鄭愉，就讀五專期間對程式設計及網頁深感興趣，因此開始自學前端技術，如HTML、CSS、Javascript、AJAX、PHP等技術... 並且在五專四年級的專題報告應用所學，一個人打造了個"線上測驗平台"，並且獲得評審高分，專題報告分數為全班第二。<br><br>五專畢業以後進入了小驢行股份有限公司，開始接觸了前端框架Vue，以及各種前端的前沿技術，比如動態路由、打包及模組化設計等等... 在那邊也參與開發過許多專案，包括大型系統的前台、後台的獨立開發，經過一年的磨練學會了非常多東西、克服了非常多困難，為我前端身涯當中非常重要的里程碑。<br><br>
+        之後進入了中華龍網自身實力更是大幅成長，也逐漸往全端工程師的方向發展，除了維護及開發新產品，更是學到了AWS、Linux、Docker、CI/CD等等實用技能，這些都是許多前端工程師缺乏的技能，讓我一個專案從規劃、開發、建置環境到部署上線都可以一手包辦，相信必定能成為貴公司傑出的助力!
+      </div>
     </div>
     <div class="indexItem PORTFOLIO animate__animated" v-scrollIn="'animate__bounceIn'">
       <Title content="PORTFOLIO"></Title>
